b0550a0945448f910b45dac71b7c1d1d44e409c9
Jhon Alexander <Alexx.16@protonmail.com>
2024-06-05 13:16:01 -0500

    clean

--- orfeo/adodb/adodb-doc.inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-     session_start();
-	$nomfile="orfeoReport-".date("Y-m-d").".doc"; 	
-	header("Content-type: application/msword; ");
-	header("Content-Disposition: filename=\"$nomfile\";");
-	include("adodb-basedoc.inc.php");
-?>
--- orfeo/documentacion/phpxref/phpxref.bat
+++
@@ -1,5 +0,0 @@
-REM *** Script to run PHPXref under Windows without having to open
-REM *** a command prompt
-
-C:\Perl\bin\perl phpxref.pl
-pause
